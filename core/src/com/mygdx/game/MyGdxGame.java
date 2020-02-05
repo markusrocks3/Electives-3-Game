c7f1f6c54967b4c89d4933aa5c4fd84f10b4389f
Mark Adrian Valencia <m.adrian.val@@gmail.com>
2020-02-05 12:18:36 +0800

    Mark Adrian Valencia
    implemented socket io

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,14 +6,18 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private Socket socket;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		connectSocket();
 	}
 
 	@Override
@@ -24,7 +28,16 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+	public void connectSocket(){
+		try{
+		socket = IO.socket("http://localhost:8080");
+		socket.connect();
+		} catch (Exception e) {
+		System.out.println(e);
+		}
+
+
+	}
 	@Override
 	public void dispose () {
 		batch.dispose();
